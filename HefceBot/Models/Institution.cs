bc09f9db4cef4c28e879e0067e0e36eed2e226a3
jclementson <julian.clementson@eduserv.org.uk>
2016-06-17 11:54:20 +0100

    API call to get list of institutions (first 25 so far)

---
+++ HefceBot/Models/Institution.cs
@@ -0,0 +1,18 @@
+﻿namespace HefceBot.Models
+{
+    public class Institution
+    {
+        public string ApiUrl { get; set; }
+        public string Country { get; set; }
+        public int NSSQuestion24 { get; set; }
+        public int NSSQuestion24Population { get; set; }
+        public string Name { get; set; }
+        public int NumberOfCourses { get; set; }
+        public string PUBUKPRN { get; set; }
+        public string QAAReportUrl { get; set; }
+        public string SortableName { get; set; }
+        public string StudentUnionUrl { get; set; }
+        public string StudentUnionUrlWales { get; set; }
+        public string UKPRN { get; set; }
+    }
+}
---
+++ HefceBot/Services/UnistatsService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using HefceBot.Models;
+
+namespace HefceBot.Services
+{
+    public class UnistatsService
+    {
+        private readonly string _unistatsEndpoint;
+        private readonly string _unistatsAuthKey;
+
+        public UnistatsService()
+        {
+            _unistatsEndpoint = ConfigurationManager.AppSettings["UnistatsEndpoint"];
+            _unistatsAuthKey = ConfigurationManager.AppSettings["UnistatsAuthKey"];
+        }
+
+        public IEnumerable<Institution> GetInstitutions()
+        {
+            string auth = $"{_unistatsAuthKey}:";
+            var encodedAuth = Convert.ToBase64String(Encoding.UTF8.GetBytes(auth));
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_unistatsEndpoint);
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", encodedAuth);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = client.GetAsync("Institutions").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    return response.Content.ReadAsAsync<IEnumerable<Institution>>().Result;
+                }
+            }
+
+            return null;
+        }
+    }
+}
