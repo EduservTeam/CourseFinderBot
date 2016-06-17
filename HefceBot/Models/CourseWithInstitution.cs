273e4ee969bc09bcf676f23647a0221370ae84ee
jclementson <julian.clementson@eduserv.org.uk>
2016-06-17 12:47:16 +0100

    Add method to get courses by institution and all courses

---
+++ HefceBot/Models/CourseWithInstitution.cs
@@ -0,0 +1,16 @@
+﻿namespace HefceBot.Models
+{
+    public class CourseWithInstitution : Course
+    {
+        public Institution Institution { get; set; }
+
+        public CourseWithInstitution(Course course)
+        {
+            ApiUrl = course.ApiUrl;
+            KisCourseId = course.KisCourseId;
+            KisMode = course.KisMode;
+            Title = course.Title;
+            TitleInWelsh = course.TitleInWelsh;
+        }
+    }
+}
